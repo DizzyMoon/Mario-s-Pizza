b3ba2208552a6f24622e69cb2be414608f65d810
tomm8231 <tomm8231@stud.kea.dk>
2022-04-20 10:58:07 +0200

    switch case added

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -1,10 +1,41 @@
 package com.company;
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
 
 public class Controller {
 
   public void run(){
+    Scanner sc = new Scanner(System.in);
+    int input = sc.nextInt();
+
+    switch (input) {
+      case 1 -> showMenu();
+      case 2 -> showOrder();
+      case 3 -> showOrderHistory();
+      case 4 -> addOrder();
+      case 5 -> setOrderStatus();
+    }
+
+  }
+
+  public void showMenu() {
+
+  }
+
+  public void showOrder() {
+
+  }
+
+  public void showOrderHistory() {
+
+  }
+
+  public void addOrder() {
+
+  }
+
+  public void setOrderStatus() {
     String spacing = createSpacing("Hawaii pizza");
     System.out.println("Hawaii Pizza" + spacing);
   }
