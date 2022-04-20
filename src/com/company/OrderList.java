c05c73ed57bcc8d14d0c1d331eb2c1b7452ab108
mads94i5 <mads94i5@stud.kea.dk>
2022-04-20 10:58:44 +0200

    OrderList og OrderHistory

--- src/com/company/OrderList.java
+++ src/com/company/OrderList.java
@@ -4,4 +4,16 @@ import java.util.ArrayList;
 
 public class OrderList {
     private ArrayList<Order> orders = new ArrayList<>();
+
+    public ArrayList<Order> getOrders() {
+        return orders;
+    }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
+    public void removeOrder(Order order) {
+        orders.remove(order);
+    }
 }
