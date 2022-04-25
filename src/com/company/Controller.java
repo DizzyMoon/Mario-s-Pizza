4bc0118e3ee04000788cd099180cbac985389e4f
DizzyMoon <mikkel.simons1@gmail.com>
2022-04-25 22:30:54 +0200

    Fixed off by one bug in sortOrderList

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -296,11 +296,9 @@ public class Controller {
 
     private ArrayList<Order> sortOrderList(OrderList input) {
         ArrayList<Order> list = input.getOrders();
-        int hourA = 0;
-        int hourB = 0;
 
         for (int i = 0; i < list.size(); i++) {
-            for (int j = 1; j < ((list.size() - 1) - i); j++) {
+            for (int j = 1; j < (list.size() - i); j++) {
                 if (compareOrders(list.get(j - 1), list.get(j))) {
                     Order temp = list.get(j - 1);
                     list.set(j - 1, list.get(j));
